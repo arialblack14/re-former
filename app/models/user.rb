f76df9bccdab34d6684c2dd33a20e2cdbc009023
arialblack14 <tsirasalexis@yahoo.gr>
2015-12-07 23:24:36 +0200

    Add user_params method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, uniqueness: true, length: { maximum: 28 }
+  validates :username, presence: true, length: { maximum: 28 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     uniqueness: { case_sensitive: false }, 
                     format: { with: VALID_EMAIL_REGEX }
+
+  validates :password, presence: true, length: { minimum: 6 }
 end
